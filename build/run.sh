2463d6adb4a1079dd2fa21d6d3d2411fac840cf8
David Gleich <david@familie-gleich.de>
2019-09-27 19:50:47 +0200

    Switch to busybox

--- build/run.sh
+++ build/run.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-additional_cmd=""
- BASE_URL="http://localhost:8080/"
-
-if [[ "${ENV}" == "dev" ]]; then
-    additional_cmd="-D"
-elif [[ "${ENV}" == "prod" ]]; then
-    additional_cmd="--environment production"
-fi
-
-if [[ -n "$URL" ]]; then
-    BASE_URL="${URL}"
-fi
-
-hugo server --port ${PORT} --bind=0.0.0.0 --disableLiveReload ${additional_cmd} --appendPort=false --baseURL ${BASE_URL} 
+busybox httpd -h /public -p ${PORT} -f
